6026c0f7f4f80697a4663f0e2b695adba0dcafb7
Chaitanya <127930753+chaitanya-ranjan29@users.noreply.github.com>
2023-10-13 15:24:30 +0530

    Update FormA.js

--- app/FormA.js
+++ app/FormA.js
@@ -2,14 +2,21 @@
 import { useState } from "react";
 
 function FormA({ onSubmit, age }) {
+
+  const [show, setShow] = useState('');
+
     const handleSubmit = (event) => {
+      event.preventDefault();
+      onSubmit(show);
+
     };
+    console.log({show});
     return (
         <form id="dc" onSubmit={handleSubmit}>
             <h2>Form A</h2>
             <label>
                 Select DC Shows:
-                <select  >
+                <select onChange={(e) => setShow(e.target.value)} >
                     <option value="">--Select--</option>
                     <option value="The Flash">The Flash</option>
                     <option value="Arrow">Arrow</option>
