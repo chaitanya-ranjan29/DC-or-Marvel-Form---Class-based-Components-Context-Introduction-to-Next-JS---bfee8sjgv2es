7413bd0302488c43c741047128b2a3e64d500d3e
Chaitanya <127930753+chaitanya-ranjan29@users.noreply.github.com>
2023-10-13 15:25:13 +0530

    Update Summary.js

--- app/Summary.js
+++ app/Summary.js
@@ -1,15 +1,24 @@
 'use client'
 import { useState } from "react";
-function Summary({ formData }) {
+function Summary({ formData, age, step }) {
+    console.log("formData", formData);
+    console.log("age", age);
+    console.log("step", step);
+
     return (
         <div id="summary">
             <h2>Summary</h2>
-            <p>Form Type: </p>
-            <p>Age: </p>
+            <p>Form Type: {formData.formType} </p>
+            <p>Age: {formData.age} </p>
 
-            <p>DC Shows: </p>
+            
+            {formData.formType=="Form A" && <p>DC Shows: {formData.showName} </p>}
+            
+            {formData.formType=="Form B" && <p>Marvel Shows: {formData.showName} </p>}
 
-            <p>Marvel Shows: </p>
+            
+
+            
 
         </div>
     );
